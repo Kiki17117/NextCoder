99da0c7eb9cea16c6d9cb985aadfe7d4b4d11c24
Kiki17117 <117hinoki@Gmail.com>
2025-07-30 22:27:20 +0900

    init commit

--- NextCoder/src/main/java/com/example/NextCoder/controller/MainController.java
+++ NextCoder/src/main/java/com/example/NextCoder/controller/MainController.java
@@ -2,22 +2,24 @@ package com.example.NextCoder.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.NextCoder.NextCoderApplication;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class MainController {
-	
-	@RequestMapping("/")
-	public String main(Model model) {
-		model.addAttribute("message","こんにちは");
-		return  "index";
-	}
-	
+
+	@GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("message", "こんにちは、Spring MVC！");
+        return "hello"; // → resources/templates/hello.html を表示
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(NextCoderApplication.class, args);
 	}
